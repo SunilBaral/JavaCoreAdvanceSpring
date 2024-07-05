30238aa66320f9e313d86ca8cc280a23cd4d202a
SunilBaral <164546740+SunilBaral@users.noreply.github.com>
2024-07-05 23:21:43 +0530

    Add files via upload

---
+++ SpringSecurityDbAppFinal/src/main/java/com/becoder/SpringSecurityDbAppFinalApplication.java
@@ -0,0 +1,13 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDbAppFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDbAppFinalApplication.class, args);
+	}
+
+}
